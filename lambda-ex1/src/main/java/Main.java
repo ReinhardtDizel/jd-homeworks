e7f8934816b59d982bd45f2e3cc321a9c01d2c74
Michael <dizel882@gmail.com>
2022-02-19 22:19:29 +0300

    Update Main.java

--- lambda-ex1/src/main/java/Main.java
+++ lambda-ex1/src/main/java/Main.java
@@ -10,6 +10,10 @@ public class Main {
         int b = calc.minus.apply(1, 1);
 
         try {
+             /*
+             * В методе возникает ошибка деления на 0. Решить эту проблему можно, только не создавая такую проблему, потому, что деление на 0 не допустимо.
+             * Поэтому будем выкидывать исключение в основную программу, использующую класс Calculator, пусть она решает, как ей поступить
+             */
             int c = calc.devide.apply(a, b);
             calc.println.accept(c);
         } catch (ArithmeticException exception) {
