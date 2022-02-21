f1393025cc51ab6a3ee7b554dc36c4ff485d0887
Michael <dizel882@gmail.com>
2022-02-21 18:25:08 +0300

    Исаправилл клласс Worker метод start
    ошибка не в 33 методах а в 33м методе

--- lambda-ex2/src/main/java/Worker.java
+++ lambda-ex2/src/main/java/Worker.java
@@ -12,11 +12,12 @@ public class Worker {
     }
 
     public void start() {
-        for (int i = 1; i <= 67; i++) {
-            callback.onDone("Task " + i + " is done");
-        }
-        for (int i = 68; i <= 100; i++) {
-            errorCallback.onError("Task " + i + " is error");
+        for (int i = 0; i < 100; i++) {
+            if (i != 33) {
+                callback.onDone("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Task " + i + " is error");
+            }
         }
     }
 }
