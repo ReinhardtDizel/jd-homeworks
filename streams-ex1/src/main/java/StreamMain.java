1db4c5615fc53d15fcfaa2e5bf4d67e8c841f255
Michael <dizel882@gmail.com>
2022-02-23 10:05:18 +0300

    Задача 1: Работа с числами

--- streams-ex1/src/main/java/StreamMain.java
+++ streams-ex1/src/main/java/StreamMain.java
@@ -1,6 +1,7 @@
 package main.java;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamMain {
@@ -8,5 +9,11 @@ public class StreamMain {
     public static void main(String... args) {
 
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList
+                .stream()
+                .filter(integer -> integer > 0)
+                .filter(integer -> integer % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
     }
 }
